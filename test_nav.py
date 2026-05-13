506dd592d368496588b904da4159f40a03ef903c
CarlotaMarto <carlota.marto@gmail.com>
2026-05-13 19:41:52 +0100

    a

--- test_nav.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-try:
-    st.page_link(st.Page("app.py", title="Home", url_path="home"), label="Home")
-    st.page_link(st.Page("pages/1_Distributions.py", title="Distributions", url_path="distributions"), label="1. Distributions")
-    st.write("st.Page objects worked!")
-except Exception as e:
-    st.write("Error:", str(e))
